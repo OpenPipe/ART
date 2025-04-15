af28611c41fcf0d1018e6f424d3900493d272568
Kyle Corbitt <kyle@corbt.com>
2025-04-15 15:58:40 -0700

    Allow logging of non-trainable models

--- src/art/local/api.py
+++ src/art/local/api.py
@@ -1,12 +1,7 @@
-import httpx
 import json
 import math
 from mp_actors import move_to_child_process
 import numpy as np
-from openai import (
-    AsyncOpenAI,
-    DefaultAsyncHttpxClient,
-)
 import os
 import polars as pl
 import subprocess
@@ -21,7 +16,7 @@ from .. import dev
 from ..model import Model, TrainableModel
 from .service import ModelService
 from ..trajectories import Trajectory, TrajectoryGroup
-from ..types import TrainableModelName, Message, TrainConfig
+from ..types import Message, TrainConfig
 from ..utils import format_message
 from .pack import (
     packed_tensors_from_tokenized_results,
@@ -59,17 +54,20 @@ class LocalAPI:
         self._tokenizers: dict[str, "PreTrainedTokenizerBase"] = {}
         self._wandb_runs: dict[str, Run] = {}
 
-    async def register_for_training(
+    async def register(
         self,
         model: Model,
     ):
         """
-        Retrieve an existing model or create a new one.
+        Registers a model with the local API for logging and/or training.
 
         Args:
             model: An art.Model instance.
         """
-        os.makedirs(self._get_output_dir(model), exist_ok=True)
+        output_dir = self._get_output_dir(model)
+        os.makedirs(output_dir, exist_ok=True)
+        with open(f"{output_dir}/model.json", "w") as f:
+            json.dump(model.model_dump(), f)
 
     async def _get_service(self, model: TrainableModel) -> ModelService:
         if model.name not in self._services:
@@ -179,28 +177,23 @@ class LocalAPI:
             print(f'No "{benchmark}" metric found in history')
         delete_checkpoints(output_dir, steps_to_keep)
 
-    async def _get_openai_client(
+    async def _prepare_backend_for_training(
         self,
         model: TrainableModel,
         config: dev.OpenAIServerConfig | None,
-    ) -> AsyncOpenAI:
+    ) -> tuple[str, str]:
         service = await self._get_service(model)
         await service.start_openai_server(config=config)
         server_args = (config or {}).get("server_args", {})
-        return AsyncOpenAI(
-            base_url=f"http://{server_args.get('host', '0.0.0.0')}:{server_args.get('port', 8000)}/v1",
-            api_key=server_args.get("api_key", "default"),
-            http_client=DefaultAsyncHttpxClient(
-                timeout=httpx.Timeout(timeout=1200, connect=5.0),
-                limits=httpx.Limits(
-                    max_connections=100_000, max_keepalive_connections=100_000
-                ),
-            ),
-        )
+
+        base_url = f"http://{server_args.get('host', '0.0.0.0')}:{server_args.get('port', 8000)}/v1"
+        api_key = server_args.get("api_key", None) or "default"
+
+        return base_url, api_key
 
     async def _log(
         self,
-        model: TrainableModel,
+        model: Model,
         trajectory_groups: list[TrajectoryGroup],
         split: str = "val",
     ) -> None:
@@ -209,7 +202,10 @@ class LocalAPI:
             for j, trajectory in enumerate(group):
                 if isinstance(trajectory, BaseException):
                     continue
-                directory = f"{self._get_output_dir(model)}/trajectories/{split}/{self.__get_step(model):04d}"
+                if isinstance(model, TrainableModel):
+                    directory = f"{self._get_output_dir(model)}/trajectories/{split}/{self.__get_step(model):04d}"
+                else:
+                    directory = f"{self._get_output_dir(model)}/trajectories/{split}"
                 os.makedirs(directory, exist_ok=True)
                 i_digits = len(str(len(trajectory_groups) - 1))
                 j_digits = len(str(len(group) - 1))
